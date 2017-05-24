f79b0001938fdc44645a42d15c149b6e55f53d2e
JannesLauwers <janneslau@hotmail.com>
2017-05-24 10:18:10 +0200

    Test commit

--- src/main/java/be/cegeka/battle/Soldier.java
+++ src/main/java/be/cegeka/battle/Soldier.java
@@ -1,12 +1,14 @@
 package be.cegeka.battle;
 
-import org.apache.commons.lang3.Validate;
-
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import org.apache.commons.lang3.Validate;
 
 public class Soldier {
 
     private String name;
+
+    private String weapon;
 
     public Soldier(String name) {
         Validate.isTrue(isNotBlank(name));
